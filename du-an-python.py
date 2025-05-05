f71ac4da4ee7667cde7f0596976f868197224266
ha-dung-1085 <hadung1085@github.com>
2025-05-05 19:59:17 +0700

    thêm và sửa file

---
+++ du-an-python.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+Tạo tài khoản cloud trên GITHUB Từ tài khoản mail outlook
+Tạo folder dùng chung trên GITHUB
+Link tài khoản
+https://github.com/ha-dung-1085/du-an-dung-chung
+
+Cài đặt ứng dụng Vs code, python, github → lưu ý: tích chọn và xác định Path lưu dữ liệu trong bước cài đặt
+Cài đặt extension python, git, jupyter trong VS code
+
+Tải folder dùng chung về máy tính/mac→ code
+git clone https://github.com/username/TEN FOLDER.git
+
+Vào đường dẫn đến folder dùng chung
+CD "tên folder dùng chung"
+
+Cập nhật thay đổi lên GITHUB→code
+git add .
+hoặc
+git add "tên file.đuôi mở rộng"
+git commit -m "thông tin thay đổi"
+git push origin main
+
+Cập nhật thay đổi từ GITHUB về máy tính/mac
+git pull origin main
